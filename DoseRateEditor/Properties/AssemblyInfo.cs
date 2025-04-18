c20055f7d54e6348b179bc982a64c2949ac1900e
Ryan Clark <112436993+Varian-RC@users.noreply.github.com>
2025-04-17 20:33:09 -0700

    Changed exp date

--- DoseRateEditor/Properties/AssemblyInfo.cs
+++ DoseRateEditor/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyExpirationDate("1/1/2024")]
+[assembly: AssemblyExpirationDate("1/1/2026")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
